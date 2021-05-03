bed5ffd3a19559342639c7323b403b58ac41fcdc
melburne <melburne42@gmail.com>
2021-05-03 12:41:49 +0530

    Add Cloud Function

---
+++ src/main/java/org/example/GcsEvent.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Date;
+
+public class GcsEvent {
+  // Cloud Functions uses GSON to populate this object.
+  // Field types/names are specified by Cloud Functions
+  // Changing them may break your code!
+  private String bucket;
+  private String name;
+  private String metageneration;
+  private Date timeCreated;
+  private Date updated;
+  
+  public String getBucket() {
+    return bucket;
+  }
+  
+  public void setBucket(String bucket) {
+    this.bucket = bucket;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getMetageneration() {
+    return metageneration;
+  }
+  
+  public void setMetageneration(String metageneration) {
+    this.metageneration = metageneration;
+  }
+  
+  public Date getTimeCreated() {
+    return timeCreated;
+  }
+  
+  public void setTimeCreated(Date timeCreated) {
+    this.timeCreated = timeCreated;
+  }
+  
+  public Date getUpdated() {
+    return updated;
+  }
+  
+  public void setUpdated(Date updated) {
+    this.updated = updated;
+  }
+}
